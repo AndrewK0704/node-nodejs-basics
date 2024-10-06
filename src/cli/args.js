00ecfbae7fff0bb371d157fb2e5d0df4a59dc0a7
andrewk0704 <andrew.kozhenov@mail.ru>
2024-10-06 16:27:45 +0400

    feat: add solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
     // Write your code here 
+    let str='';
+
+    for (let i=2; i<process.argv.length; i=i+2){
+        str=str+process.argv[i].slice(2)+' is '+process.argv[i+1]+', ';
+    }
+    console.log(str.slice(0, str.length-2));
 };
 
 parseArgs();
