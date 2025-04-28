90817a9a5ea8335d148656abe81cd11777abdd80
andrewk0704 <andrew.kozhenov@mail.ru>
2025-04-28 20:28:36 +0400

    feat: add solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,14 @@
+import {Worker} from "worker_threads";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const performCalculations = async () => {
     // Write your code here
+
+    const worker = new Worker(path.join(path.dirname(fileURLToPath(import.meta.url)), "worker.js"), {workerData: 10});
+    worker.on('message', msg=>console.log(msg))
+
 };
 
 await performCalculations();
